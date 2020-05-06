8ed5fdc62ab0ee7bfc01cc4fac2fb161a7775249
coder2k@mail.ru <standalone!@##@!!@#>
2020-05-06 11:05:45 +0300

    Initial blog

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+'''
+    todo:
+        [ ] - create CORE app
+        [ ] - make model for blog
+        [ ] - Category with title and slug
+        [ ] - Tag - title and slug
+        [ ] - Post - title, slug, author, content, created_at, photo, view, category, tags
+        [ ] - install pillow package
+        [ ] - create git
+        [ ] - create super user
+'''
+
