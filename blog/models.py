5f45a5c986383d5a04ffe724907e4cd3b86e8076
coder2k@mail.ru <standalone!@##@!!@#>
2020-05-06 12:09:35 +0300

    Созданы модели:  -категории  -поста  -тэга

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,62 @@
 from django.db import models
 
 # Create your models here.
+from core.models import TimeStampedModel
+
 '''
     todo:
         [x] - create CORE app
-        [ ] - make model for blog
-        [ ] - Category with title and slug
-        [ ] - Tag - title and slug
-        [ ] - Post - title, slug, author, content, created_at, photo, view, category, tags
-        [ ] - install pillow package
+        [x] - make model for blog
+        [x] - Category with title and slug
+        [x] - Tag - title and slug
+        [x] - Post - title, slug, author, content, created_at, photo, view, category, tags
+        [x] - install pillow package
         [x] - create git
         [ ] - create super user
 '''
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок категории')
+    slug = models.SlugField(unique=True, max_length=255, verbose_name='Url')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Имя тэга')
+    slug = models.SlugField(unique=True, max_length=255, verbose_name='Url')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['title']
+
+
+class Post(TimeStampedModel):
+    title = models.CharField(max_length=100, verbose_name='Заголовок поста'),
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='Url')
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField(blank=True, verbose_name='Пост')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фотография')
+    view = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts',
+                                 verbose_name='Категория')
+    tags = models.ManyToManyField(Tag, related_name='Тэги', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-updated']
