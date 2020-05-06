9af9c7fd0dbdfe4c189d6d6ee30c1c4d11da301e
coder2k@mail.ru <standalone!@##@!!@#>
2020-05-06 11:24:23 +0300

    Создан абстрактный класс Time stamp

--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 '''
     todo:
-        [ ] - create CORE app
+        [x] - create CORE app
         [ ] - make model for blog
         [ ] - Category with title and slug
         [ ] - Tag - title and slug
         [ ] - Post - title, slug, author, content, created_at, photo, view, category, tags
         [ ] - install pillow package
-        [ ] - create git
+        [x] - create git
         [ ] - create super user
 '''
 
---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from blog import views as blog_views
+
+app_name = "core"
+
+urlpatterns = [
+    path("", blog_views.index, name="home")
+]
