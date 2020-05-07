daadd3f333584a0cef40d1d584263736909a188c
coder2k@mail.ru <standalone!@##@!!@#>
2020-05-07 11:20:44 +0300

    + list_display полей для админки + save_as для упрощения добавления по шаблону + вернуть в списке полей миниатюру фотографии + добавить поиск в админки + добавить фильтр + поля только для чтения - views + добавить fieldsets или fields

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from . import models
 
 
@@ -26,7 +28,55 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
+    save_as = True
     save_on_top = True
     prepopulated_fields = {"slug": ("title",)}
     filter_horizontal = ['tags']
+    list_display_links = ['title', 'slug']
+    list_filter = ['tags', 'category']
+    search_fields = ['title',
+                     'author',
+                     'content',
+                     'photo',
+                     'view', ]
     form = PostAdminForm
+
+    list_display = [
+        'title',
+        'slug',
+        'author',
+        'content',
+        'view',
+        'category',
+        'created',
+        'updated',
+        'get_photo'
+    ]
+
+    fieldsets = (
+        (None, {
+            'fields': ('title',
+                       'slug',
+                       'author',
+                       'content',
+                       'category',
+                       'tags',
+                       'view',
+                       'created',
+                       'updated',
+                       )
+        }),
+        ('Загрузить фотографию', {
+            'classes': ('wide',),
+            'fields': ('get_photo','photo')
+        }),
+    )
+
+    readonly_fields = ['view', 'created', 'updated', 'get_photo']
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src={obj.photo.url} width=50/>")
+        return "-"
+
+    get_photo.short_description = "Превью"
--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class TimeStampedModel(models.Model):
     """ Abstract model for time stamps """
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата Обновления')
 
     class Meta:
         abstract = True
