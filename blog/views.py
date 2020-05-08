b50b294edee9caf9079ef3f4d8eee2abad35037c
coder2k@mail.ru <standalone!@##@!!@#>
2020-05-08 09:26:55 +0300

    + сделать пагинацию на странице постов + сделать вывод масимум по 3 станицы + сделать стрелку перемещения

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from . import models
 
 
-# Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
 
 
 def category(request, slug):
     return render(request, 'blog/index.html')
+
+
+class Home(ListView):
+    model = models.Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 8
+    paginate_orphans = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница мега блока'
+        return context
--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from blog import views as blog_views
 app_name = "core"
 
 urlpatterns = [
-    path("", blog_views.index, name="home")
+    path("", blog_views.Home.as_view(), name="home")
 ]
