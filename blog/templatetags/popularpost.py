55284c9968509aaeee430e1ab28da4c639b7e566
coder2k@mail.ru <standalone!@##@!!@#>
2020-05-09 11:44:57 +0300

    Добавлен вывод постов через кастомный тэг

---
+++ blog/templatetags/popularpost.py
@@ -0,0 +1,10 @@
+from django import template
+from blog.models import Post
+
+register = template.Library()
+
+
+@register.inclusion_tag('blog/popposts.html')
+def popularpost(cnt=3):
+    popposts = Post.objects.order_by('-view')[:cnt]
+    return {"popposts": popposts}
